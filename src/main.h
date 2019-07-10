de67983a24e50bdbda0dba13d009d7670f86356d
Mike Sowter <mike@sowter.com>
2019-07-10 14:21:49 +1000

    partially built

---
+++ src/main.h
@@ -0,0 +1,58 @@
+#include <arduino.h>
+#include <TimeLib.h>
+#include <fs.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266FTPServer.h>
+#include <WiFiUdp.h>
+#include <ESP8266mDNS.h>
+#include <Ticker.h>
+#include <ArduinoOTA.h>
+#include "secrets.h"
+#include <RCSwitch.h>
+extern "C" {
+#include "user_interface.h"
+}
+
+ESP8266WebServer server( 80 );
+FtpServer ftpSrv;
+RCSwitch mySwitch = RCSwitch();
+WiFiUDP udp;
+WiFiClient client;
+time_t getNtpTime();
+FSInfo fs_info;
+File fh,fd,fe;
+Ticker secondTick;
+volatile int watchDog = 0;
+
+char fileName[] = "/XXyymmdd.csv";
+char alarmText[40];
+unsigned long getTime();
+unsigned long sendNTPrequest(IPAddress& address);
+unsigned long getNTPreply();
+String resetReason = "restart: " + ESP.getResetReason();
+char d2Str[] = "12";
+char d8Str[] = "12345.78";
+char hexStr[]= "      ";
+char thisText[6],lastText[]="12345";
+uint8_t repCount=0;
+
+unsigned long t0, t1, minMillis, startMillis, startSeconds, midNight;
+unsigned int localPort = 2391;   //  a random local port to listen for UDP packets
+
+IPAddress localIP,timeServerIP,fileServerIP;
+IPAddress ip(192, 168, 1, 55);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(192, 168, 1, 1);
+const char* ntpServerName = "au.pool.ntp.org";
+const int HTML_SIZE = 10000;
+char htmlStr[HTML_SIZE];        // use C strings for storage efficiency
+int htmlLen;
+const int NTP_PACKET_SIZE = 48;
+const int BUFFER_SIZE = 128;
+const int ISR_CAP = 128;
+byte buffer[BUFFER_SIZE];
+char outBuf[128];               // for ftpRcv and errMessage
+const int TIMEZONE = 10;
---
+++ src/main.ino
@@ -0,0 +1,58 @@
+
+#include <RCSwitch.h>
+#include <main.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\n433MHz monitor V2.0  2019-07-10");
+	// join local network and internet
+	joinNet();
+	// setup over the air updates
+	init_OTA();
+	// setup watch dog
+	secondTick.attach(1,ISRwatchDog);
+	// Set epoch and timers
+	setupTime();
+	// setup server responses
+	setupServer();
+	// setup file system and diag files
+	setupSPIFFS();
+	// setup FTP server
+	ftpSrv.begin("mike","iron");
+  // setup radio interface
+  mySwitch.enableReceive(5);      // Receiver on interrupt 0 => that is pin #2
+	// lookup reason for restart
+	resetReason.toCharArray(charBuf,resetReason.length()+1);
+  diagMess(charBuf);              // restart message
+	startMillis = millis();
+}
+
+void loop() {
+  if (mySwitch.available()) {
+    timeStamp();
+    output(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
+    mySwitch.resetAvailable();
+    delay(5000);
+  }
+  watchDog=0;
+  // check for OTA
+  ArduinoOTA.handle();
+  server.handleClient();
+  fh.flush();
+}
+
+void ISRwatchDog () {
+  noInterrupts();
+  watchDog++;
+	if (watchDog == 30) {
+    Serial.println("watchDog 30s alert");
+  }
+  if (watchDog >= 60) {
+    Serial.println("watchDog 60s reset");
+    fh.close();
+    ESP.restart();
+  }
+  interrupts();
+}
